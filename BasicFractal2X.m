2e43dd3e1bf3476098c8a9c1159c8a357937a0a6
Karthik <Shaji>
2021-02-21 15:57:37 -0800

    Angle Rotation
    Now can create fractals with any number of stems, changing the rotation angles to produce more interesting shapes

--- BasicFractal2X.m
+++ BasicFractal2X.m
@@ -1,31 +1,19 @@
 figure
 hold on %keep data
-angleIts = 4; %amount of times this repeats essentially
-angle = 0;
-redFactor = 0.80;
+
+angleIts = 36; %amount of times broader for loop repeats
+angle = 0; %starting angle
+redFactor = 0.99; %how much the line distance shrinks
+masterDist = 2;
+angRotation = 360 / angleIts;
 for i = 1: angleIts
-    stPoint = [0, 0];
-    dist1 = 1;
-    angle = angle + 360 / angleIts; %intended for later ability to change angle amount
+    stPoint = [0, 0]; %each line starts at the origin
+    dist1 = masterDist;
+    angle = angle + angRotation; %intended for later ability to change angle amount
     for j = 1: 100
-        %the below execution will change as I fit more angles
-        if angle == 90
-            P2 = [stPoint(1) + dist1, stPoint(2)];
-            plotTwoPoints(stPoint, P2);
-            
-        elseif angle == 180
-            P2 = [stPoint(1), stPoint(2) + dist1];
-            plotTwoPoints(stPoint, P2);
-            
-        elseif angle == 270
-            P2 = [stPoint(1) - dist1, stPoint(2)];
-            plotTwoPoints(stPoint, P2);
-            
-        else
-            P2 = [stPoint(1), stPoint(2) - dist1];
-            plotTwoPoints(stPoint, P2);
-        end
-        angle = mod(angle + 90, 360);
+        P2 = [stPoint(1) + dist1 * cosd(angle), stPoint(2) + dist1 * sind(angle)];
+        plotTwoPoints(stPoint, P2);
+        angle = mod(angle + angRotation, 360);
         dist1 = dist1 * redFactor;
         stPoint = P2;    
     end
