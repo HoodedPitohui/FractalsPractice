a3a13b685cc16a3de2fc5dd06f07a4c4da17c888
Karthik <Shaji>
2021-02-21 09:52:27 -0800

    Function Simplification

--- BasicFractal.m
+++ BasicFractal.m
@@ -6,24 +6,16 @@ angle = 90;
 for i = 1: 100
     if angle == 90
         P2 = [stPoint(1) + dist1, stPoint(2)];
-        X1 = [stPoint(1), P2(1)];
-        Y1 = [stPoint(2), P2(2)];
-        plot(X1, Y1);
+        plotTwoPoints(stPoint, P2);
     elseif angle == 180
         P2 = [stPoint(1), stPoint(2) + dist1];
-        X1 = [stPoint(1), P2(1)];
-        Y1 = [stPoint(2), P2(2)];
-        plot(X1, Y1);
+        plotTwoPoints(stPoint, P2);
     elseif angle == 270
         P2 = [stPoint(1) - dist1, stPoint(2)];
-        X1 = [stPoint(1), P2(1)];
-        Y1 = [stPoint(2), P2(2)];
-        plot(X1, Y1);
+        plotTwoPoints(stPoint, P2);
     else
         P2 = [stPoint(1), stPoint(2) - dist1];
-        X1 = [stPoint(1), P2(1)];
-        Y1 = [stPoint(2), P2(2)];
-        plot(X1, Y1);
+        plotTwoPoints(stPoint, P2);
     end
     angle = mod(angle + 90, 360);
     dist1 = dist1 / 2;
