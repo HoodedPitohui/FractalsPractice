7cf34123bec6567f7c0d8ddc074986fcd489fbb1
Karthik <Shaji>
2021-02-27 09:28:24 -0800

    Single angle ridges

--- KochCurve.m
+++ KochCurve.m
@@ -1,19 +1,17 @@
 clear
 figure
 hold on
-dist = 1;
+
+dist = 2;
+degree = 0;
+
 p1 = [0, 0];
 p2 = [dist, 0];
-p3 = [dist / 3, 0];
-p4 = [dist - dist / 3, 0];
+
 counter = 1;
 limit = 2;
-drawKochLine(p1, p2, dist, counter, limit);
-test2 = (dist - dist / 3 ) - (dist / 3)
-p5 = [test2 + test2 * cosd(60), test2 * sind(60)];
-xlim([0 1])
-ylim([-1 1])
-c2 = plotTwoPointsS(p3, p5);
-c3 = plotTwoPointsS(p4, p5);
-c1 = plotTwoPointsS(p1, p3);
-c6 = plotTwoPointsS(p4, p2);
+drawKochLine(p1, p2, dist, degree, counter, limit);
+
+
+xlim([0 dist])
+ylim([-dist dist])
--- drawKochLine.m
+++ drawKochLine.m
@@ -1,7 +1,29 @@
-function [] = drawKochLine(p1, p2, dist, counter, limit)
+function [] = drawKochLine(p1, p2, dist, degree, counter, limit)
     if (counter == limit)
+        p3 = [p1(1) + dist / 3, p1(2)];
+        p4 = [p2(1) - dist / 3, p2(2)];
+        test2 = (p2(1) - dist / 3 ) - (p1(1) + dist / 3)
+        p5 = [p1(1) + test2 + test2 * cosd(60), test2 * sind(60)];
         
+        plotTwoPoints(p3, p5);
+        plotTwoPoints(p4, p5);
+        plotTwoPoints(p1, p3);
+        plotTwoPoints(p4, p2);
     elseif (counter < limit)
+        p3 = [p1(1) + dist / 3, p1(2)];
+        p4 = [p2(1) - dist / 3, p2(2)];
+        test2 = (p2(1) - dist / 3 ) - (p1(1) + dist / 3);
+        p5 = [test2 + test2 * cosd(60), test2 * sind(60)];
+        
+        plotTwoPoints(p3, p5);
+        plotTwoPoints(p4, p5);
+        %plotTwoPoints(p1, p3);
+        %plotTwoPoints(p4, p2);
+        
+        drawKochLine(p1, p3, dist / 3, degree, counter + 1, limit);
+        %drawKochLine(p3, p5, dist / 3, degree, counter + 1, limit);
+        %drawKochLine(p4, p5, dist / 3, degree, counter + 1, limit);
+        drawKochLine(p4, p2, dist / 3, degree, counter + 1, limit);
     else
     end
 end
