ff8e243a396a3def529fb3491851a0ad8278288c
Karthik <Shaji>
2021-02-27 18:21:01 -0800

    Just drew all the lines of original

--- KochCurve.m
+++ KochCurve.m
@@ -7,10 +7,15 @@ degree = 0;
 
 p1 = [0, 0];
 p2 = [dist, 0];
+p3 = [0 + cosd(-60) * dist, 0 + sind(-60) * dist];
+
+%plotTwoPoints(p1, p3);
+%plotTwoPoints(p1, p2);
+%plotTwoPoints(p2, p3);
 
 counter = 1;
 limit = 2;
-drawKochLine(p1, p2, dist, degree, counter, limit);
+%drawKochLine(p1, p2, dist, degree, counter, limit);
 
 
 xlim([0 dist])
